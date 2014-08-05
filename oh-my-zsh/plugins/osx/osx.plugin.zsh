9da5a01d58aaa0d88983d2763cbc8edbff68d2e0
Kevin Colyar <kevin@colyar.net>
2014-08-05 11:33:34 -0700

    Updates oh-my-zsh

--- oh-my-zsh/plugins/osx/osx.plugin.zsh
+++ oh-my-zsh/plugins/osx/osx.plugin.zsh
@@ -157,3 +157,40 @@ function trash() {
 function vncviewer() {
   open vnc://$@
 }
+
+# iTunes control function
+function itunes() {
+	local opt=$1
+	shift
+	case "$opt" in
+		launch|play|pause|stop|rewind|resume|quit)
+			;;
+		mute)
+			opt="set mute to true"
+			;;
+		unmute)
+			opt="set mute to false"
+			;;
+		next|previous)
+			opt="$opt track"
+			;;
+		vol)
+			opt="set sound volume to $1" #$1 Due to the shift
+			;;
+		""|-h|--help)
+			echo "Usage: itunes <option>"
+			echo "option:"
+			echo "\tlaunch|play|pause|stop|rewind|resume|quit"
+			echo "\tmute|unmute\tcontrol volume set"
+			echo "\tnext|previous\tplay next or previous track"
+			echo "\tvol\tSet the volume, takes an argument from 0 to 100"
+			echo "\thelp\tshow this message and exit"
+			return 0
+			;;
+		*)
+			print "Unknown option: $opt"
+			return 1
+			;;
+	esac
+	osascript -e "tell application \"iTunes\" to $opt"
+}
