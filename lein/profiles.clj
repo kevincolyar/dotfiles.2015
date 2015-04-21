7acd2836166bacfc78ce72f75abd6330cfc4cc3b
Kevin Colyar <kevin@colyar.net>
2015-04-21 10:48:25 -0700

    Adds lein profiles.clj

---
+++ lein/profiles.clj
@@ -0,0 +1,6 @@
+{:user {:plugins [
+                  [venantius/ultra "0.3.3"]
+                  [jonase/eastwood "0.2.1"] ;; Lint tool
+                  ]
+        :ultra {:color-scheme :solarized_dark}}}
+
